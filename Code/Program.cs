2ceec720f02fd218878b6bb4fdbd15840dee16c8
boogie <76754631+BoogieHasFun@users.noreply.github.com>
2022-02-17 22:33:31 -0500

    Add files via upload

--- Code/Program.cs
+++ Code/Program.cs
@@ -8,7 +8,24 @@ namespace A_Program
         {
             Console.WriteLine("Hello, the man with the sword.. What is your name?");
             Console.ReadLine();
-            Console.WriteLine("Great! Lets started.");
+            Console.WriteLine("Great Name! What do you like most: Games or Coding?");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Nice!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("You can edit this all you want on https://github.com/BoogieHasHun/Boogies-CMD-Story!");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\r   ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Click any key to exit.");
+            
+            Console.ReadKey();
         }
     }  
 }
